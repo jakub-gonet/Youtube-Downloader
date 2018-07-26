c4f031c4883fa20d77d8ddf425ca607e28123cba
Jakub Gonet <jakub.gonet0@gmail.com>
2018-07-26 18:37:53 +0200

    Add function for downloading media from given link

--- lib/yt_downloader/yt_utility.ex
+++ lib/yt_downloader/yt_utility.ex
@@ -42,6 +42,41 @@ defmodule YtUtility do
   def parse(""), do: {:error, :empty_link}
   def parse(_), do: {:error, :unrecognized_link}
 
+  @doc """
+  Downloads a given link to given file type and returns path where file was saved.
+
+  `file_type` is `bestvideo`, `bestaudio`, `worstvideo` or `worstaudio`
+  `file_type` can be also one of the numbers returned by `get_available_formats/1`
+  or `3gp`, `aac`, `flv`, `m4a`, `mp3`, `mp4`, `ogg`, `wav`, `webm` extentions (if available of course).
+  """
+  def download(url = @playlist_url <> _, file_type),
+    do: _download(url, @playlist_template, file_type)
+
+  def download(url = @watch_url <> _, file_type), do: _download(url, @video_template, file_type)
+
+  defp _download(link, file_template, file_type) do
+    args = List.flatten([@downloader_options, "-o" <> file_template, "-f " <> file_type, link])
+
+    {json, error_code} =
+      System.cmd(
+        @downloader,
+        args
+      )
+
+    with 0 <- error_code,
+         {:ok, metadata} <- Poison.decode(json),
+         filepath <- extract_file_path_from_map(metadata) do
+      {metadata, filepath}
+    else
+      1 -> {:error, :downloader_error}
+      error -> error
+    end
+  end
+
+  defp extract_file_path_from_map(metadata) do
+    Map.get(metadata, "_filename", {:error, :missing_entry})
+  end
+
   defp extract_param_from_link(arguments_string, param) do
     arguments_string
     |> URI.decode_query()
