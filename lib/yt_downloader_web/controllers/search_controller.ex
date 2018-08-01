a3d9c18a8b71e764db9ac776aeb77dd8baa588de
Jakub Gonet <jakub.gonet0@gmail.com>
2018-08-01 11:13:52 +0200

    Implement SearchController's search function

--- lib/yt_downloader_web/controllers/search_controller.ex
+++ lib/yt_downloader_web/controllers/search_controller.ex
@@ -2,6 +2,13 @@ defmodule YtDownloaderWeb.SearchController do
   use YtDownloaderWeb, :controller
 
   def search(conn, %{"search" => %{"q" => query}}) do
-    render conn, "search.html", query: query
+    if {:ok, type} = YtUtility.valid_link?(query) do
+      case type do
+        :video -> redirect conn, to: video_path(conn, :show, query)
+        :playlist -> redirect conn, to: playlist_path(conn, :show, query)
+      end
+    else
+      render(conn, "search.html", search_results: YtUtility.search(query))
+    end
   end
 end
