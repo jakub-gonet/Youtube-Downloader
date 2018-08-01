84742af7d9e0bdbdd2b7e8f0b924594777066a2d
Jakub Gonet <jakub.gonet0@gmail.com>
2018-08-01 21:41:44 +0200

    Add functions in view extracting data from search result

--- lib/yt_downloader_web/views/search_view.ex
+++ lib/yt_downloader_web/views/search_view.ex
@@ -1,3 +1,18 @@
 defmodule YtDownloaderWeb.SearchView do
   use YtDownloaderWeb, :view
+
+  def get_searched_query(query)
+  def get_searched_query(%{"search_info" => %{"query" => q}}), do: q
+
+  def extract_video_data(search_result)
+
+  def extract_video_data(%{"snippet" => video_data}) do
+    %{
+      channel: video_data["channelTitle"],
+      title: video_data["title"],
+      desc: video_data["description"],
+      thumbnail: get_in(video_data, ["thumbnails", "medium", "url"]),
+      download_page: "#"
+    }
+  end
 end
