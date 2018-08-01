055dc69079a4cc43c2f53836e1f14a1721a5989c
Jakub Gonet <jakub.gonet0@gmail.com>
2018-08-01 11:18:46 +0200

    Simplify search function

--- lib/yt_downloader_web/controllers/search_controller.ex
+++ lib/yt_downloader_web/controllers/search_controller.ex
@@ -2,13 +2,17 @@ defmodule YtDownloaderWeb.SearchController do
   use YtDownloaderWeb, :controller
 
   def search(conn, %{"search" => %{"q" => query}}) do
-    if {:ok, type} = YtUtility.valid_link?(query) do
-      case type do
-        :video -> redirect conn, to: video_path(conn, :show, query)
-        :playlist -> redirect conn, to: playlist_path(conn, :show, query)
-      end
-    else
-      render(conn, "search.html", search_results: YtUtility.search(query))
+    case YtUtility.valid_link?(query) do
+      {:ok, type} ->
+        redirect_to(conn, query, type)
+
+      :error ->
+        render(conn, "search.html", search_results: YtUtility.search(query))
     end
   end
+
+  defp redirect_to(conn, query, :video), do: redirect(conn, to: video_path(conn, :show, query))
+
+  defp redirect_to(conn, query, :playlist),
+    do: redirect(conn, to: playlist_path(conn, :show, query))
 end
