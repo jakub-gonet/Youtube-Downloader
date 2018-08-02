221df69d24f4fa75f54f293ac619b7badb8618c9
Jakub Gonet <jakub.gonet0@gmail.com>
2018-08-02 18:48:28 +0200

    Add :show action for video and playlist controllers

--- lib/yt_downloader_web/controllers/playlist_controller.ex
+++ lib/yt_downloader_web/controllers/playlist_controller.ex
@@ -1,3 +1,7 @@
 defmodule YtDownloaderWeb.PlaylistController do
   use YtDownloaderWeb, :controller
+
+  def show(conn, %{"id" => id}) do
+    render(conn, "video.html", id: id)
+  end
 end
--- lib/yt_downloader_web/controllers/video_controller.ex
+++ lib/yt_downloader_web/controllers/video_controller.ex
@@ -1,3 +1,7 @@
 defmodule YtDownloaderWeb.VideoController do
   use YtDownloaderWeb, :controller
+
+  def show(conn, %{"id" => id}) do
+    render(conn, "video.html", id: id)
+  end
 end
