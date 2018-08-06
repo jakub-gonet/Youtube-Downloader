71dd2557a10eaa9572497acfcc1aeae5196f904b
Jakub Gonet <jakub.gonet0@gmail.com>
2018-08-06 17:44:27 +0200

    Fix typo
    Playlist controlller was rendering video.html instead of playlist.html

--- lib/yt_downloader_web/controllers/playlist_controller.ex
+++ lib/yt_downloader_web/controllers/playlist_controller.ex
@@ -2,6 +2,6 @@ defmodule YtDownloaderWeb.PlaylistController do
   use YtDownloaderWeb, :controller
 
   def show(conn, %{"id" => id}) do
-    render(conn, "video.html", id: id)
+    render(conn, "playlist.html", id: id)
   end
 end
