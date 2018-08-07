63e47c3e1e6f48f6c3788e9c206acb33e8ffec99
Jakub Gonet <jakub.gonet0@gmail.com>
2018-08-07 13:42:35 +0200

    Remove functions in view
    This is replaced with YtUtility.get_video_data/2

--- lib/yt_downloader_web/views/search_view.ex
+++ lib/yt_downloader_web/views/search_view.ex
@@ -4,23 +4,4 @@ defmodule YtDownloaderWeb.SearchView do
   def get_searched_query(query)
   def get_searched_query(%{"search_info" => %{"query" => q}}), do: q
 
-  def extract_video_data(search_result)
-
-  def extract_video_data(query = %{"snippet" => video_data}) do
-    %{
-      channel: video_data["channelTitle"],
-      title: video_data["title"],
-      desc: video_data["description"],
-      thumbnail: get_in(video_data, ["thumbnails", "medium", "url"]),
-      id: get_id(query)
-    }
-  end
-
-  def kind(%{"kind" => "youtube#playlist"}), do: :playlist
-  def kind(%{"kind" => "youtube#video"}), do: :video
-  def kind(_), do: :error
-
-  defp get_id(%{"id" => id}) do
-    Map.get(id, "playlistId") || Map.get(id, "videoId")
-  end
 end
