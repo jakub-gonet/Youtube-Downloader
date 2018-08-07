b764c9c064d72b29c8f02c6e39f16b0728b1f2bc
Jakub Gonet <jakub.gonet0@gmail.com>
2018-08-07 13:45:12 +0200

    Move logic from view to controller

--- lib/yt_downloader_web/controllers/search_controller.ex
+++ lib/yt_downloader_web/controllers/search_controller.ex
@@ -1,5 +1,6 @@
 defmodule YtDownloaderWeb.SearchController do
   use YtDownloaderWeb, :controller
+  require Logger
 
   def search(conn, %{"search" => %{"q" => query}}) do
     case YtUtility.get_url_type(query) do
@@ -7,7 +8,32 @@ defmodule YtDownloaderWeb.SearchController do
         redirect_to(conn, query, type)
 
       :other ->
-        render(conn, "search.html", search_results: YtUtility.search(query, "video,playlist"))
+        get_id = fn :get, data, _next -> data["playlistId"] || data["videoId"] end
+
+        get_type = fn :get, data, _next ->
+          case data do
+            "youtube#playlist" -> :playlist
+            "youtube#video" -> :video
+            _ -> :error
+          end
+        end
+
+        wanted = [
+          id: ["id", get_id],
+          type: ["id", "kind", get_type],
+          title: ["snippet", "title"],
+          channel: ["snippet", "channelTitle"],
+          desc: ["snippet", "description"],
+          thumbnail: ["snippet", "thumbnails", "medium", "url"]
+        ]
+
+        results =
+          query
+          |> YtUtility.search("video,playlist")
+          |> Map.get("items")
+          |> Enum.map(&YtUtility.extract_video_data(&1, wanted))
+
+        render(conn, "search.html", search_results: results)
     end
   end
 
