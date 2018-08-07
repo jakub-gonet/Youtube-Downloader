3de751cb23e7758f96e235fa7081b7d351506765
Jakub Gonet <jakub.gonet0@gmail.com>
2018-08-07 20:55:44 +0200

    Implement show action in video controller

--- lib/yt_downloader_web/controllers/video_controller.ex
+++ lib/yt_downloader_web/controllers/video_controller.ex
@@ -2,6 +2,21 @@ defmodule YtDownloaderWeb.VideoController do
   use YtDownloaderWeb, :controller
 
   def show(conn, %{"id" => id}) do
-    render(conn, "video.html", id: id)
+    wanted = [
+      id: ["id"],
+      title: ["snippet", "title"],
+      channel: ["snippet", "channelTitle"],
+      channel_id: ["snippet", "channelId"],
+      desc: ["snippet", "description"]
+    ]
+
+    results =
+      id
+      |> YtUtility.get_video_data()
+      |> Map.get("items")
+      |> List.first()
+      |> YtUtility.extract_video_data(wanted)
+
+    render(conn, "video.html", data: results)
   end
 end
